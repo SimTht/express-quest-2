ae1756a9e397904d284fe80ced96dbe5a634ed64
SIMON THOUET <simon.thouet@gmail.com>
2022-11-07 19:22:34 +0000

    Challenge solution quest 3

--- userHandlers.js
+++ userHandlers.js
@@ -30,7 +30,25 @@ const getUserById = (req, res) => {
     });
 };
 
+const postUser = (req, res) => {
+  const { firstname, lastname, city, language, email } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, city, language, email) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, city, language, email]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
+  postUser
 };
